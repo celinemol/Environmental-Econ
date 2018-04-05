38e01065f1878daea23669d483a2ed04345cbe8a
Celine Mol <Celine@169-231-169-103.wireless.ucsb.edu>
2018-04-05 15:54:46 -0700

    Testing and initial notes

--- ESM204_Assignment1.R
+++ ESM204_Assignment1.R
@@ -1,2 +1,13 @@
 ## Celine Mol and Tom Paschos
 ## ESM 204 Homework #1
+
+## Import Dataset
+dat <- read_excel("HW1data.xlsx")
+colnames(dat) <- c("parcel","a","b","cost")
+
+## Initial Notes
+# Suppose a total of 50 parcels (of equal size) are being considered for purchase to conserve two species of interest
+N <- 50 # Sample size 
+
+# C = cost of acquiring 100% of that parcel
+# A & B = index for habitat quality for a species
